1c2b9f5b02e3a326dd6869781c8ac2e3be9f4028
Your Name <annisaauliamaulida03@gmail.com>
2024-06-05 12:44:17 +0700

    CRUD PRODI

--- app/Http/Controllers/ProdiController.php
+++ app/Http/Controllers/ProdiController.php
@@ -30,10 +30,40 @@ class ProdiController extends Controller
             [
                 'nama_prodi' => 'required|unique:prodi|max:255'
             ],
+            [
+                'nama_prodi.required' => 'Nama Prodi harus diisi',
+                'nama_prodi.unique' => 'Nama Prodi sudah ada',
+                'nama_prodi.max' => 'Nama Prodi maksimal 255 karakter'
+            ]
         );
             Prodi::create($validateData);
             return redirect ('/prodi');
     }
+
+    public function edit(String $id)
+    {
+        $data = ['nama' => 'maulida', 'foto' =>'E020322089.jpg'];
+        $prodi = Prodi::find($id);
+        return view('prodi.edit', compact(['data', 'prodi']));
+    }
+    public function update(Request $request, string $id)
+    {
+        $validateData = $request->validate(
+            [
+                'nama_prodi' => 'required|unique:prodi|max:255'
+            ],
+            [
+                'nama_prodi.required' => 'Nama Prodi harus diisi',
+                'nama_prodi.unique' => 'Nama Prodi sudah ada',
+                'nama_prodi.max' => 'Nama Prodi maksimal 255 karakter'
+            ]
+        );
+        Prodi::where('id' , $id)->update($validateData);
+        return redirect('/prodi');
+    }
+    public function destroy(string $id)
+    {
+        Prodi::destroy($id);
+        return redirect('/prodi');
+    }
 }
-
-
--- resources/views/prodi/edit.blade.php
+++ resources/views/prodi/edit.blade.php
@@ -1,17 +1,17 @@
 @extends('template.main')
 
 @section('content')
-
+<!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <div class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0">Tambah Program Studi</h1>
+            <h1 class="m-0">Edit Program Studi</h1>
           </div><!-- /.col -->
           <div class="col-sm-6">
-            <ol class="breadcrumb afloat-sm-right">
+            <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item active"></li>
             </ol>
           </div><!-- /.col -->
@@ -19,7 +19,7 @@
       </div><!-- /.container-fluid -->
     </div>
     <!-- /.content-header -->
-
+  
     <!-- Main content -->
     <section class="content">
         <div class="container-fluid">
@@ -27,28 +27,25 @@
             <!-- left column -->
             <div class="col-md-12">
               <!-- jquery validation -->
-              <div class="card card-warning">
+              <div class="card card-primary">
                 <div class="card-header">
-                  <h3 class="card-title">Tambah Data Prodi</h3>
+                  <h3 class="card-title">Edit Data Prodi</h3>
                 </div>
                 <!-- /.card-header -->
                 <!-- form start -->
-                <form action="{{url('prodi')}}" method="POST">
+                <form action="{{ url("prodi/$prodi->id") }}" method="post">
+                    @method('put')
                     @csrf
                   <div class="card-body">
                     <div class="form-group">
-                      <label for="nama_prodi">Nama Prodi</label>
-                      <input type="text" name="nama_prodi" class="form-control @error('nama_prodi') is-invalid @enderror" id="nama_prodi" placeholder="Masukkan Prodi">
-                      @error('nama_prodi')
-                        <div class="invalid-feedback">
-                          {{ $message }}
-                        </div>
-                      @enderror
+                      <label for="namaprodi">Nama Prodi</label>
+                      <input type="text" name="nama_prodi" class="form-control" id="namaprodi" 
+                      value="{{  $prodi->nama_prodi }}">
                     </div>
                   </div>
                   <!-- /.card-body -->
                   <div class="card-footer">
-                    <button type="submit" class="btn btn-warning">Submit</button>
+                    <button type="submit" class="btn btn-primary">Submit</button>
                   </div>
                 </form>
               </div>
@@ -57,7 +54,7 @@
             <!--/.col (left) -->
             <!-- right column -->
             <div class="col-md-6">
-
+  
             </div>
             <!--/.col (right) -->
           </div>
@@ -66,5 +63,4 @@
       </section>
     <!-- /.content -->
   </div>
-
 @endsection
